ca5e78628df66a5011918ebd3e1638a00b96f5f1
JokerGho5t <didgai.1997@gmail.com>
2026-07-15 23:43:07 +0300

    UI/UX: Added icon for LWComponent

--- Source/LevelWriter/Private/Core/LWComponent.cpp
+++ Source/LevelWriter/Private/Core/LWComponent.cpp
@@ -1,7 +1,37 @@
 ﻿#include "Core/LWComponent.h"
 #include "Core/LWEventScript.h"
 
-ULWComponent::ULWComponent() { PrimaryComponentTick.bCanEverTick = false; }
+#if WITH_EDITORONLY_DATA
+#include "Components/BillboardComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Engine/Texture2D.h"
+#endif
+
+ULWComponent::ULWComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+#if WITH_EDITORONLY_DATA
+	EditorSprite = CreateEditorOnlyDefaultSubobject<UBillboardComponent>(TEXT("EditorSprite"));
+	if (EditorSprite)
+	{
+		EditorSprite->SetupAttachment(this);
+		
+		static ConstructorHelpers::FObjectFinder<UTexture2D> SpriteTexture(TEXT("/LevelWriter/Icons/Icon128.Icon128"));
+		if (SpriteTexture.Succeeded())
+		{
+			EditorSprite->Sprite = SpriteTexture.Object;
+		}
+		
+		EditorSprite->bHiddenInGame = true;
+		EditorSprite->bIsScreenSizeScaled = true;
+
+		EditorSprite->SetDepthPriorityGroup(SDPG_Foreground);
+		
+		EditorSprite->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
+	}
+#endif
+}
 
 void ULWComponent::BeginPlay()
 {
@@ -20,6 +50,19 @@ void ULWComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 		if (Event) Event->Deinitialize();
 	}
 	Super::EndPlay(EndPlayReason);
+}
+
+void ULWComponent::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+#if WITH_EDITORONLY_DATA
+	if (EditorSprite)
+	{
+		EditorSprite->DestroyComponent();
+		EditorSprite = nullptr;
+	}
+#endif
+
+	Super::OnComponentDestroyed(bDestroyingHierarchy);
 }
 
 void ULWComponent::AbortAllEvents()
--- Source/LevelWriter/Public/Core/LWComponent.h
+++ Source/LevelWriter/Public/Core/LWComponent.h
@@ -1,12 +1,13 @@
 ﻿#pragma once
 
-#include "Components/ActorComponent.h"
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
 #include "LWComponent.generated.h"
 
 class ULWEventScript;
 
 UCLASS(ClassGroup= "LevelWriter", DisplayName= "LevelWriter Component", meta=(BlueprintSpawnableComponent, PrioritizeCategories="LevelWriter"))
-class LEVELWRITER_API ULWComponent : public UActorComponent
+class LEVELWRITER_API ULWComponent : public USceneComponent
 {
 	GENERATED_BODY()
 public:
@@ -18,6 +19,14 @@ public:
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+	virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+
 	UFUNCTION(BlueprintCallable, Category = "LevelWriter")
 	void AbortAllEvents();
+
+#if WITH_EDITORONLY_DATA
+	
+	UPROPERTY()
+	TObjectPtr<UBillboardComponent> EditorSprite;
+#endif
 };
